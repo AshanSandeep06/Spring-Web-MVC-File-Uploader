e1efde01c9ffe379809ac4355a18d3ff0dd777b1
AshanSandeep06 <ashansandeep06@gmail.com>
2023-02-08 15:46:29 +0530

    Added FileUploadController class and mapping to a Url

---
+++ Back-End/src/main/java/lk/ijse/spring/controller/FileUploadController.java
@@ -0,0 +1,12 @@
+package lk.ijse.spring.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/file_upload")
+public class FileUploadController {
+
+}

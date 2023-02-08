622160223e122ba6ac68e3bf9ca4d890b2533def
AshanSandeep06 <ashansandeep06@gmail.com>
2023-02-08 16:49:44 +0530

    Content Updated in WebAppConfig Class

--- Back-End/src/main/java/lk/ijse/spring/config/WebAppConfig.java
+++ Back-End/src/main/java/lk/ijse/spring/config/WebAppConfig.java
@@ -1,12 +1,14 @@
 package lk.ijse.spring.config;
 
+import lk.ijse.spring.advisor.AppWideExceptionHandler;
+import lk.ijse.spring.controller.FileUploadController;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan()
+@ComponentScan(basePackageClasses = {FileUploadController.class, AppWideExceptionHandler.class})
 public class WebAppConfig {
 
 }
--- Back-End/src/main/java/lk/ijse/spring/controller/FileUploadController.java
+++ Back-End/src/main/java/lk/ijse/spring/controller/FileUploadController.java
@@ -1,8 +1,9 @@
 package lk.ijse.spring.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lk.ijse.spring.util.ResponseUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin

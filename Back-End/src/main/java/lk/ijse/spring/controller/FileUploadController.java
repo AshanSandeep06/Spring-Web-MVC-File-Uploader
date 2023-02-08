27f5de441457c230e43637835b84bfb8dd5b5e9c
AshanSandeep06 <ashansandeep06@gmail.com>
2023-02-08 16:55:21 +0530

    Content Updated

--- Back-End/src/main/java/lk/ijse/spring/controller/FileUploadController.java
+++ Back-End/src/main/java/lk/ijse/spring/controller/FileUploadController.java
@@ -9,5 +9,8 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin
 @RequestMapping("/file_upload")
 public class FileUploadController {
-
+    @GetMapping
+    public ResponseEntity<ResponseUtil> getFile(String id) {
+        return new ResponseEntity<ResponseUtil>(new ResponseUtil("OK", "Successfully Received", id), HttpStatus.OK);
+    }
 }

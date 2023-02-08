b18c96b76291d8586efa4f1b8e98dedd6e2b144f
AshanSandeep06 <ashansandeep06@gmail.com>
2023-02-08 18:10:36 +0530

    Added Dependencies for File Uploading

--- Back-End/src/main/java/lk/ijse/spring/config/WebAppConfig.java
+++ Back-End/src/main/java/lk/ijse/spring/config/WebAppConfig.java
@@ -2,13 +2,20 @@ package lk.ijse.spring.config;
 
 import lk.ijse.spring.advisor.AppWideExceptionHandler;
 import lk.ijse.spring.controller.FileUploadController;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackageClasses = {FileUploadController.class, AppWideExceptionHandler.class})
 public class WebAppConfig {
-
+    @Bean
+    public CommonsMultipartResolver multipartResolver(){
+        CommonsMultipartResolver resolver = new CommonsMultipartResolver();
+        resolver.setMaxUploadSize(100000);
+        return resolver;
+    }
 }

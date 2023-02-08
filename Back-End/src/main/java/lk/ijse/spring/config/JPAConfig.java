7b9b2e17f23514e72d714423a2410c23e57b83f6
AshanSandeep06 <ashansandeep06@gmail.com>
2023-02-08 14:21:49 +0530

    Annotations were settled in Configuration Classes

--- Back-End/src/main/java/lk/ijse/spring/config/JPAConfig.java
+++ Back-End/src/main/java/lk/ijse/spring/config/JPAConfig.java
@@ -1,4 +1,13 @@
 package lk.ijse.spring.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories()
 public class JPAConfig {
+
 }
--- Back-End/src/main/java/lk/ijse/spring/config/WebRootConfig.java
+++ Back-End/src/main/java/lk/ijse/spring/config/WebRootConfig.java
@@ -1,4 +1,12 @@
 package lk.ijse.spring.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import(JPAConfig.class)
+@ComponentScan()
 public class WebRootConfig {
+
 }
